5a6c4d73aa1712c48ee661cda0d6fca235652db4
rovi9969 <motwani.ravi20@gmail.com>
2025-06-09 15:39:34 +0530

    Create Task 1: Perform Basic Mathematical Operations.py
    Problem Statement: Write a Python program that does the following:
    
    1.  Takes two numbers as input from the user.
    2.  Performs the basic mathematical operations on these two numbers:
    
    o	Addition
    o	Subtraction
    o	Multiplication
    o	Division
    
    3.  Displays the results of each operation on the screen.
     Expected Output:
    The output should include the result of each operation performed, for example:
    
    Enter the first number: 5
    Enter the second number: 10
    
    Addition:  15
    Subtraction:  -5
    Multiplication:  50
    Division:  0.5

---
+++ Task 1: Perform Basic Mathematical Operations.py
@@ -0,0 +1,16 @@
+num1 = input('Enter the first number: ')
+num2 = input('Enter the second number: ')
+
+num1 = int(num1)
+num2 = int(num2)
+
+addition_value = num1+num2
+subtraction_value = num1-num2
+multiplication_value = num1*num2
+division_value = num1/num2
+
+print('\n')
+print('Addition: ', addition_value)
+print('Subtraction: ', subtraction_value)
+print('Multiplication: ', multiplication_value)
+print('Division: ', division_value)
