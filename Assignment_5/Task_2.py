9b6bf63ce529ae05f2f1d475041d334bed09685c
rovi9969 <motwani.ravi20@gmail.com>
2025-06-10 15:15:08 +0530

    Create Task_2.py
    Task 2: Demonstrate List Slicing 
    Problem Statement: Write a Python program that:
    1.   Creates a list of numbers from 1 to 10.
    2.   Extracts the first five elements from the list.
    3.   Reverses these extracted elements.
    4.   Prints both the extracted list and the reversed list
     
    Expected Output:
    Original list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
    Extracted first five elements:  [1, 2, 3, 4, 5]
    Reverse extracted elements: [5, 4, 3, 2, 1]

---
+++ Assignment_5/Task_2.py
@@ -0,0 +1,11 @@
+# Task 2: Demonstrate List Slicing 
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(f"Original list: {numbers}")
+
+extracted_numbers = numbers[:5]
+print("Extracted first five elements: ", extracted_numbers)
+
+extracted_numbers.reverse()
+print("Reverse extracted elements:", extracted_numbers)
