c33fe9435bf346d9c488755346252e9990bfbc10
rovi9969 <motwani.ravi20@gmail.com>
2025-06-10 11:11:49 +0530

    Update Task_2.py

--- Assignment_4/Task_2.py
+++ Assignment_4/Task_2.py
@@ -1,15 +1,18 @@
 # Task 2: Write and Append Data to a File
 
-with open('output.txt', 'w') as file:
+file_path = "output.txt"
+with open(file_path, 'w') as file:
     # Write user input to the file
     write_data = input("Enter text to write to the file: ")
     file.write(write_data)
+    print(f"Data successfully written to {file_path}.\n")
 
-with open('output.txt', 'a') as file:
+with open(file_path, 'a') as file:
     # Append user input to the file
     append_data = input("Enter additional text to append: ")
     file.write('\n' + append_data)
+    print(f"Data successfully appended.\n")
 
-with open('output.txt', 'r') as file:
+with open(file_path, 'r') as file:
     read_file = file.read()
     print(f"Final content of output.txt:\n{read_file}")
